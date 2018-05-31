5a0ad56117efcc790a0d5f5ce4536e076345ff01
Val Mandantes <mandantesv@gmail.com>
2018-05-31 09:21:54 +0800

    Add exclude.list to installer (i forgot)
    Signed-off-by: Val Mandantes <mandantesv@gmail.com>

--- config.sh
+++ config.sh
@@ -87,6 +87,7 @@ set_permissions() {
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
   cp -af $INSTALLER/common/aapt $MODPATH/aapt
+  cp -af $INSTALLER/common/aapt $MODPATH/exclude.list
   bin=bin
   if (grep -q samsung /system/build.prop); then
     bin=xbin
@@ -97,6 +98,7 @@ set_permissions() {
   fi
   set_perm $MODPATH/system/$bin/debloat 0 0 0777
   set_perm $MODPATH/aapt 0 0 0755
+  set_perm $MODPATH/exclude.list 0 0 0644
 }
 
 ##########################################################################################
